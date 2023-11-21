379082fae5193fe42b28d3836dec823fa187fad1
Doni-githu <Doni-githu@users.noreply.github.com>
2023-11-21 01:53:35 +0000

    deploy: 886962a3f8a9d4f8e9370279231619ed9a688dc8

---
+++ assets/Footer-ZakZK-uF.js
@@ -0,0 +1,7 @@
+import{r as e,_ as o,j as t}from"./index-l-PaefyQ.js";const r=e.lazy(()=>o(()=>import("./FooterLeftSide-gRGzTH6H.js"),__vite__mapDeps([0,1,2]))),s=e.lazy(()=>o(()=>import("./FooterRightSide-Zpf9Bnn2.js"),__vite__mapDeps([3,1,2,4]))),_=()=>t.jsx("div",{className:"footer",children:t.jsxs(e.Suspense,{children:[t.jsx(r,{}),t.jsx(s,{})]})});export{_ as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/FooterLeftSide-gRGzTH6H.js","assets/index-l-PaefyQ.js","assets/index-q028ATsE.css","assets/FooterRightSide-Zpf9Bnn2.js","assets/telegram-ZIi8I8qz.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ assets/Header-CfGNg7EL.js
@@ -0,0 +1,7 @@
+import{r,_ as e,j as s}from"./index-l-PaefyQ.js";const t=r.lazy(()=>e(()=>import("./Navbar-YdhKkmik.js"),__vite__mapDeps([0,1,2,3]))),a=r.lazy(()=>e(()=>import("./Showcase-W4Z1tf9D.js"),__vite__mapDeps([4,1,2]))),_=()=>s.jsx("div",{className:"top",children:s.jsxs(r.Suspense,{children:[s.jsx(t,{}),s.jsx(a,{})]})});export{_ as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/Navbar-YdhKkmik.js","assets/index-l-PaefyQ.js","assets/index-q028ATsE.css","assets/telegram-ZIi8I8qz.js","assets/Showcase-W4Z1tf9D.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
