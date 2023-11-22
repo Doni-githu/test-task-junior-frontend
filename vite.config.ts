bc012886274a1f897c61c9a3ac5a91a7a51a2718
Doniyor <ddonierov96@gmail.com>
2023-11-22 12:20:04 +0500

    end

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
+    base: '/',
   }
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
   return config
 })
